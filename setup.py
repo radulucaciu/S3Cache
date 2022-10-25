f6e1a01a6dcdfe17baedb8fe3c24cf1b22a9abcd
Radu <>
2022-10-25 12:51:31 +0300

    Updated dependencies and version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
    name='s3cache',
-   version='0.1',
+   version='0.2',
    description='A module that Caches Database queries to S3',
    author='Radu Lucaciu',
    author_email='',
    packages=['s3cache'],  #same as name
-   install_requires=['pandas', 'sqlalchemy'], #external packages as dependencies
+   install_requires=['fastparquet', 'pandas', 'sqlalchemy'], #external packages as dependencies
 )
